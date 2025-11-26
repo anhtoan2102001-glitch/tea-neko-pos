5de056b472eff5a81e6faea6795565b3b685e5d6
anhtoan2102001-glitch <anhtoan2102001@gmail.com>
2025-11-27 00:55:55 +0900

    MINI-POS

---
+++ js/supabase.js
@@ -0,0 +1,7 @@
+// js/supabase.js
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+export const supabase = createClient(
+  "https://kcrchwedlrettpkucoct.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtjcmNod2VkbHJldHRwa3Vjb2N0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM4ODQ0MjgsImV4cCI6MjA3OTQ2MDQyOH0.g4WA9tci2zm4MIa6EvOe6tH_EhaoicdrdiP6swgjRgw"
+);
